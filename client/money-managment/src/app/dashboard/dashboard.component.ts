3ae9b382edd5dce74ca8e2221360b183a223a002
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-08-17 21:01:35 +0300

    add logic

--- client/money-managment/src/app/dashboard/dashboard.component.ts
+++ client/money-managment/src/app/dashboard/dashboard.component.ts
@@ -7,15 +7,59 @@ import { Category } from '../classes/category';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-savings:Category[];
-expenses:Category[];
+  savings: Category[];
+  expenses: Category[];
+  selectedSpend: number;
+  selectedSaving: number;
+  selectedIncome: boolean;
 
-  constructor( public authService: AuthService) {
-    this.savings=authService.user.savings;
-    this.expenses=authService.user.expenses
-   }
+  constructor(public authService: AuthService) {
+    this.savings = authService.user.savings;
+    this.expenses = authService.user.expenses;
+    this.selectedIncome = false;
+    this.selectedSaving = -1;
+    this.selectedSpend = -1;
+  }
 
   ngOnInit(): void {
+  }
+
+  selectSpend(index: number) {
+    if (this.selectedSaving != -1) {
+      this.selectedSpend = -1;
+      this.selectedSaving = -1;
+      confirm('Spend money)))');
+    }
+    else {
+      alert('First choose saving');
+      this.selectedSpend = -1;
+      this.selectedSaving = -1;
+      this.selectedIncome = false;
+    }
+  }
+
+  selectSaving(index: number) {
+    if (this.selectedIncome) {
+      this.selectedSaving = -1;
+      this.selectedIncome = false;
+      confirm('Income money)))');
+    }
+    else {
+      if (this.selectedSaving !== index) {
+        this.selectedSaving = index;
+        this.selectedIncome = false;
+        this.selectedSpend = -1;
+      }
+      else {
+        this.selectedSaving = -1
+      }
+    }
+  }
+
+  selectIncome() {
+    this.selectedIncome = !this.selectedIncome;
+    this.selectedSaving = -1;
+    this.selectedSpend = -1;
   }
 
 }
