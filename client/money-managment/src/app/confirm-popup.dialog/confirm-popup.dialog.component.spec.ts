d0c2ffa890072d6015645a997e8b3e9cd9bd0379
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-09-05 15:28:33 +0300

    add error

--- client/money-managment/src/app/confirm-popup.dialog/confirm-popup.dialog.component.spec.ts
+++ client/money-managment/src/app/confirm-popup.dialog/confirm-popup.dialog.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ConfirmPopup.DialogComponent } from './confirm-popup.dialog.component';
+import { ConfirmPopupDialogComponent } from './confirm-popup.dialog.component';
 
-describe('ConfirmPopup.DialogComponent', () => {
-  let component: ConfirmPopup.DialogComponent;
-  let fixture: ComponentFixture<ConfirmPopup.DialogComponent>;
+describe('ConfirmPopupDialogComponent', () => {
+  let component: ConfirmPopupDialogComponent;
+  let fixture: ComponentFixture<ConfirmPopupDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmPopup.DialogComponent ]
+      declarations: [ ConfirmPopupDialogComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ConfirmPopup.DialogComponent);
+    fixture = TestBed.createComponent(ConfirmPopupDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- client/money-managment/src/app/password-popup/password-popup.component.ts
+++ client/money-managment/src/app/password-popup/password-popup.component.ts
@@ -13,15 +13,17 @@ import {FormControl, Validators} from '@angular/forms';
 
 export class PasswordPopupComponent {
   newPassword: string;
-  hide:boolean;
-  user:User;
-  createNewPassword:boolean;
+  hide: boolean;
+  user: User;
+  createNewPassword: boolean;
+  wrongPassword:boolean;
   constructor(private authService: AuthService, public dialogRef: MatDialogRef<PasswordPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data: string) {
     this.newPassword = '';
     this.hide = true;
-    this.user=authService.user;
-    this.createNewPassword=false
+    this.user = authService.user;
+    this.createNewPassword = false;
+    this.wrongPassword=false;
   }
 
   onNoClick(): void {
@@ -31,6 +33,10 @@ export class PasswordPopupComponent {
     if(this.newPassword==this.user.password){
       this.createNewPassword=true;
     }
+    else{
+      this.wrongPassword=true;
+    }
+
   }
   password = new FormControl('',Validators.required);
   confirmPassword= new FormControl('', [Validators.required, Validators.pattern(this.password.value)]);
