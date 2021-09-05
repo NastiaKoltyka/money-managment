a076ead5b7a01a079d28ca14885031647f8d4d07
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-09-05 13:17:26 +0300

    tests for calculator

--- client/money-managment/src/app/calculator/calculator.component.spec.ts
+++ client/money-managment/src/app/calculator/calculator.component.spec.ts
@@ -23,4 +23,73 @@ describe('CalculatorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should press button', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.calculator.controls['number'].setValue('');
+    app.pressButton('3');
+    expect(app.calculator.controls['number'].value).toEqual('3');
+  });
+
+  it('should press button', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.calculator.controls['number'].setValue('');
+    app.pressButton('3');
+    expect(app.calculator.controls['number'].value).toEqual('3');
+  });
+
+  it('should evaluate expression', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.calculator.controls['number'].setValue('2+2*2');
+    app.equals();
+    expect(app.calculator.controls['number'].value).toEqual(6);
+  });
+
+  it('should save changes', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.calculator.controls['number'].setValue('15');
+    spyOn(app.apply, 'emit');
+    spyOn(app.closeCalculator, 'emit');
+    app.save();
+    expect(app.result == '15').toBeTrue();
+    expect(app.invalid_fields).toEqual(false);
+    expect(app.apply.emit).toHaveBeenCalledWith(15);
+    expect(app.closeCalculator.emit).toHaveBeenCalled();
+  });
+
+  it('shouldn\'t save changes', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.calculator.controls['number'].setValue('asd');
+    app.save();
+    expect(app.invalid_fields).toEqual(true);
+  });
+
+  it('should close', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    spyOn(app.closeCalculator, 'emit');
+    app.close();
+    expect(app.closeCalculator.emit).toHaveBeenCalled();
+  });
+
+  it('should focus', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.focused = false;
+    app.onFocus();
+    expect(app.focused).toEqual(true);
+  });
+
+  it('should blur', () => {
+    fixture = TestBed.createComponent(CalculatorComponent);
+    const app = fixture.componentInstance;
+    app.focused = true;
+    app.onBlur();
+    expect(app.focused).toEqual(false);
+  });
 });
